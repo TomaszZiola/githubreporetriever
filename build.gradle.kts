772c5318a600aefb99b06c2a6e18db8c3a3299ef
Tomasz Ziola <tomek.ziola@gmail.com>
2025-07-24 19:15:32 +0200

    cleanup in dependencies and change name of the main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm")
 }
 
 group = "com.ziola"
@@ -23,7 +22,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
