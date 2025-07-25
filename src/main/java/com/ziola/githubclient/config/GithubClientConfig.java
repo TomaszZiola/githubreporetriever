1f4e4a3eb7012e983a7dfd1bed96c5e9b722915b
Tomasz Ziola <tomek.ziola@gmail.com>
2025-07-25 10:54:44 +0200

    moved client concstants to a different file

--- src/main/java/com/ziola/githubclient/config/GithubClientConfig.java
+++ src/main/java/com/ziola/githubclient/config/GithubClientConfig.java
@@ -1,6 +1,5 @@
 package com.ziola.githubclient.config;
 
-import com.ziola.githubclient.GithubConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
