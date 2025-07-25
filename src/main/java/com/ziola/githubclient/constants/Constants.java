2b770ae1b82e7a981f8ff2c000c4099ade6a11a9
Tomasz Ziola <tomek.ziola@gmail.com>
2025-07-25 10:56:15 +0200

    moved client concstants to a different file

--- src/main/java/com/ziola/githubclient/constants/Constants.java
+++ src/main/java/com/ziola/githubclient/constants/Constants.java
@@ -1,9 +1,9 @@
-package com.ziola.githubclient;
+package com.ziola.githubclient.constants;
 
-public final class GithubConstants {
+public final class Constants {
     public static final String GITHUB_API_ACCEPT_HEADER = "application/vnd.github.v3+json";
     public static final String ACCEPT = "Accept";
 
-    private GithubConstants() {
+    private Constants() {
     }
 }
